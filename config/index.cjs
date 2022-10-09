98888c0f2624e5c343244c8ee52cd1f31fa6aaff
Y-yang-yin <115398096+Y-yang-yin@users.noreply.github.com>
2022-10-09 17:58:35 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -8,50 +8,60 @@
 const USER_CONFIG = {
 
   // 使用微信测试号：公众号APP_ID
-  APP_ID: '',
+  APP_ID: 'wx52e01a525ed5339f',
 
   // 使用微信测试号：公众号APP_SECRET
-  APP_SECRET: '',
+  APP_SECRET: '9ff0c6dc1826abd7a22ac57f6f71d984',
 
   PROVINCE: '广东',
-  CITY: '惠州',
+  CITY: '深圳',
 
   USERS: [
     {
       // 想要发送的人的名字
-      name: '宝贝',
+      name: '宝宝',
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
-      id: '',
+      id: 'o1mBs5lvduX68pQwxDvdcmvO9a6E',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
-      useTemplateId: '',
+      useTemplateId: 'BeUZehe0jBEhbT5DtSQhGiYqYfz2Dn3Mqq26CYSAr7Q',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
-      horoscopeDate: '12-27',
+      horoscopeDate: '08-28',
       festivals: [
         // 注意：此条配置日期为阴历日期，因为`type`中 “生日” 之前有 * 符号
         {
-          type: '*生日', name: '宝贝', year: '1996', date: '09-09',
+          type: '*生日', name: '宝贝', year: '1998', date: '07-07',
         },
         // 注意：此条配置日期为阳历日期，因为`type`中 “生日” 之前没有 * 符号
         {
-          type: '生日', name: '李四', year: '1996', date: '09-31',
+          type: '生日', name: '宝宝', year: '1998', date: '08-28',
         },
         {
-          type: '节日', name: '相识纪念日', year: '2020', date: '09-03',
+          type: '节日', name: '相识纪念日', year: '2022', date: '05-20',
         },
       ],
       // 我们在一起已经有xxxx天了的配置
       customizedDateList: [
         // 在一起的日子
-        { keyword: 'love_day', date: '2022-09-08' },
+        { keyword: 'love_day', date: '2022-05-20' },
         // 结婚纪念日
-        { keyword: 'marry_day', date: '2022-09-09' },
+        { keyword: 'marry_day', date: '2022-05-20' },
       ],
     },
   ],
 
 
   // 【推送完成提醒】模板id, 用来看自己有没有发送成功的那个模板
-  CALLBACK_TEMPLATE_ID: '',
+  CALLBACK_TEMPLATE_ID: '今天是：{{date.DATA}} 
+但行好事，莫问前程
+当前城市：{{city.DATA}} 
+今天的天气：{{weather.DATA}} 
+最低气温：{{min_temperature.DATA}} 
+最高气温：{{max_temperature.DATA}} 
+今天是2022年的第{{love_day.DATA}}天 
+{{birthday1.DATA}}
+
+{{note_en.DATA}} 
+{{note_ch.DATA}} ',
 
   CALLBACK_USERS: [
     {
